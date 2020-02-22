afb1ea3ba229e4a86bbc503fc572c6d056edcae0
Alex <alexcouts18@gmail.com>
2020-02-22 12:36:04 -0500

    ccomppleted header section

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,34 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+
+// creates the parent container and adds class and appends to parent element
+let headerContainer = document.createElement('div');
+headerContainer.classList.add('header');
+
+// selects the parent element and appends parent container
+let parentElement = document.querySelector('.header-container');
+parentElement.appendChild(headerContainer);
+
+// creates the child span and adds class and appends to parent container
+// and adds text content
+let date = document.createElement('span');
+date.classList.add('date');
+date.textContent = "SMARCH 28, 2019";
+headerContainer.appendChild(date);
+
+// creates header element and adds content and append to parent container 
+let title = document.createElement('h1');
+title.textContent = "Lambda Times";
+headerContainer.appendChild(title);
+
+// creates temperature span and adds class and append to parent container 
+let temp = document.createElement('span');
+temp.classList.add("temp");
+temp.textContent = '98°';
+headerContainer.appendChild(temp);
+
+}
+
+Header();
